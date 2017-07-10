9794cba05b6ba832f0bd8d8cda8086e21cc7a48d
Quentin de Longraye <quentin@dldl.fr>
2017-07-10 13:12:11 +0200

    annotate interfaces

--- src/ChatbotPlatform/MessageActionInterface.php
+++ src/ChatbotPlatform/MessageActionInterface.php
@@ -4,7 +4,23 @@ namespace dLdL\ChatbotPlatform;
 
 use dLdL\ChatbotPlatform\Event\MessageEvent;
 
+/**
+ * A message action is run when a message has been received, identified and
+ * parsed by a specific handler.
+ *
+ * An action can do anything with the message, such as saving it, sending it to
+ * a webservice, ...
+ *
+ * An action can also set a reply for the message. It will be dispatched to the
+ * handlers to be transferred, logged, ...
+ */
 interface MessageActionInterface
 {
+    /**
+     * This method is triggered when a message has been parsed by a message
+     * handler. The action may generate a reply or perform any other action.
+     *
+     * @param MessageEvent $event
+     */
     public function onMessage(MessageEvent $event): void;
 }
